bfb12ab0366d3a56a5e77a1feada79d34e875337
Brendan Cariota <brendan@cariota.com>
2021-04-17 13:23:58 -0500

    Added Login Page Layout and Transition to Register Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Image } from 'react-native'
+import { Image, KeyboardAvoidingView } from 'react-native'
 import { Button, Input } from "react-native-elements"
 import { StatusBar } from 'expo-status-bar'
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const signIn = () => {
+
+    }
+
     return (
-        <View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
             <StatusBar style="light" />
 
             <Image 
                 source={{ uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png" }}
                 style={{ width: 200, height: 200 }}
             />
-        </View>
+            <View style={styles.inputContainer}>
+                <Input 
+                    placeholder="Email"
+                    autoFocus 
+                    type="email" 
+                    value={email} 
+                    onChangeText={(text) => setEmail(text)} 
+                />
+                <Input 
+                    placeholder="Password" 
+                    secureTextEntry  
+                    type="password" 
+                    value={password} 
+                    onChangeText={(text) => setPassword(text)} 
+                />
+            </View>
+
+            <Button 
+                containerStyle={styles.button} 
+                title="Login" 
+                onPress={signIn}
+            />
+            <Button 
+                containerStyle={styles.button} 
+                type='outline' 
+                title="Register" 
+                onPress={() => navigation.navigate("Register")}
+            />
+            {/* If Register Button is touching keyboard this trick will work */}
+            {/* <View style={{ height: 100 }} /> */}
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: 'white',
+    },
+    inputContainer: {
+        width: 300,
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+})
